37c94c2b3600cb0bf8c1c2aaa4013783b9048c78
Daniele Penazzo <penazarea@altervista.org>
2018-01-17 17:10:50 +0100

    Fixed #21

--- elimina_prenotazione_admin.php
+++ elimina_prenotazione_admin.php
@@ -6,18 +6,18 @@
         if (session_status() == PHP_SESSION_NONE){
                 session_start();
         }
-        checkLoggedUser();
+        checkLoggedAdmin();
         $dbAccess = new DBAccess();
         $dbconn = $dbAccess->openDBConnection();
         if ($dbconn == false){
                 die ("Errore nella connessione al database");
         }else{
-                if ($_GET['id'] != $_SESSION['username']){
+                if ($_GET['id'] != $_SESSION['username'] && $_SESSION['admin']!=true){
                         header("Location: accesso_negato.html");
                         exit();
                 }else{
                         $dbAccess->deleteBooking($_GET['id'], $_GET['sala'], $_GET['servizio'], $_GET['data'], $_GET['ora']);
-                        header("Location: userpanel.php");
+                        header("Location: admin.php");
                 }
         }
 ?>
