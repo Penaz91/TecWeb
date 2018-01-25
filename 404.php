a91a925cd22f0457871fe943ced5b264f9b80d54
Aronoele <choccolatecakes@gmail.com>
2018-01-25 20:54:12 +0100

    modifiche pagina 404 e varie

--- 404.php
+++ 404.php
@@ -1,6 +1,10 @@
 <?php
         require_once __DIR__ . DIRECTORY_SEPARATOR . "toolkit.php";
 
-        echo(file_get_contents(__("struttura_404.html")));
+		$content = file_get_contents(__("struttura_404.html"));
+        
+        addScreenStylesheet("CSS" . DIRECTORY_SEPARATOR ."style_404.css", $content);
+        setLoadScript($content, "preparaLightbox()");
+        echo($content);
 
 ?>
