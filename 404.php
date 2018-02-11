213aae7bd4293338fdf55e5775c4e69d8197bb7e
Daniele Penazzo <penazarea@altervista.org>
2018-02-11 10:17:03 +0100

    Inserite sessioni per 404 e 500/503

--- 404.php
+++ 404.php
@@ -1,6 +1,9 @@
 <?php
         require_once __DIR__ . DIRECTORY_SEPARATOR . "toolkit.php";
 
+        if (session_status() == PHP_SESSION_NONE){
+                session_start();
+        }
         $content = file_get_contents(__("struttura_404.html"));
         addScreenStylesheet("CSS" . DIRECTORY_SEPARATOR ."style_login.css", $content);
         addMobileStyleSheet("CSS" . DIRECTORY_SEPARATOR . "style_mobile.css", $content);
